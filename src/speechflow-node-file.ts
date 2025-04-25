7e4d7c6d8d3b940f4b676f98a071871a1b72d2d5
Ralf S. Engelschall <rse@engelschall.com>
2025-04-26 01:44:17 +0200

    support stdio

--- src/speechflow-node-file.ts
+++ src/speechflow-node-file.ts
@@ -19,19 +19,25 @@ export default class SpeechFlowNodeDevice extends SpeechFlowNode {
     async open () {
         if (this.params.mode === "r") {
             this.output = this.params.type
-            this.stream = fs.createReadStream(this.params.path,
-                { encoding: this.params.type === "text" ? this.config.textEncoding : "binary" })
+            if (this.params.path === "-")
+                this.stream = process.stdin
+            else
+                this.stream = fs.createReadStream(this.params.path,
+                    { encoding: this.params.type === "text" ? this.config.textEncoding : "binary" })
         }
         else if (this.params.mode === "w") {
             this.input = this.params.type
-            this.stream = fs.createWriteStream(this.params.path,
-                { encoding: this.params.type === "text" ? this.config.textEncoding : "binary" })
+            if (this.params.path === "-")
+                this.stream = process.stdout
+            else
+                this.stream = fs.createWriteStream(this.params.path,
+                    { encoding: this.params.type === "text" ? this.config.textEncoding : "binary" })
         }
         else
             throw new Error(`invalid file mode "${this.params.mode}"`)
     }
     async close () {
-        if (this.stream !== null)
+        if (this.stream !== null && this.params.path !== "-")
             this.stream.destroy()
     }
 }
