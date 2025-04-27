d4c4072902620465dc2a41dd841f9a9b4ad3d483
Ralf S. Engelschall <rse@engelschall.com>
2025-04-27 02:25:24 +0200

    fix Deepgram node

--- src/speechflow-node-deepgram.ts
+++ src/speechflow-node-deepgram.ts
@@ -4,33 +4,48 @@
 **  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
 */
 
+/*  standard dependencies  */
 import { EventEmitter } from "node:events"
+
+/*  external dependencies  */
 import Stream           from "node:stream"
 import * as Deepgram    from "@deepgram/sdk"
+
+/*  internal dependencies  */
 import SpeechFlowNode   from "./speechflow-node"
 
+/*  SpeechFlow node for device access  */
 export default class SpeechFlowNodeDevice extends SpeechFlowNode {
+    /*  internal state  */
     private dg: Deepgram.LiveClient | null = null
+
+    /*  construct node  */
     constructor (id: string, opts: { [ id: string ]: any }, args: any[]) {
         super(id, opts, args)
+
+        /*  declare node input/output format  */
+        this.input  = "audio"
+        this.output = "text"
+
+        /*  declare node configuration parameters  */
         this.configure({
             key:      { type: "string", val: process.env.SPEECHFLOW_KEY_DEEPGRAM },
             model:    { type: "string", val: "nova-2", pos: 0 }, /* FIXME: nova-3 multiligual */
             version:  { type: "string", val: "latest", pos: 1 },
-            language: { type: "string", val: "de",     pos: 2 }
+            language: { type: "string", val: "de",  pos: 2 }
         })
     }
-    async open () {
-        this.input  = "audio"
-        this.output = "text"
-        this.stream = null
 
+    /*  open node  */
+    async open () {
         /*  sanity check situation  */
         if (this.config.audioBitDepth !== 16 || !this.config.audioLittleEndian)
             throw new Error("Deepgram node currently supports PCM-S16LE audio only")
 
-        /*  connect to Deepgram API  */
+        /*  create queue for results  */
         const queue = new EventEmitter()
+
+        /*  connect to Deepgram API  */
         const deepgram = Deepgram.createClient(this.params.key)
         this.dg = deepgram.listen.live({
             model:            this.params.model,
@@ -40,7 +55,7 @@ export default class SpeechFlowNodeDevice extends SpeechFlowNode {
             sample_rate:      this.config.audioSampleRate,
             encoding:         "linear16",
             multichannel:     false,
-            // endpointing:      false,  /* FIXME: ? */
+            endpointing:      10,
             interim_results:  false,
             smart_format:     true,
             punctuate:        true,
@@ -49,33 +64,39 @@ export default class SpeechFlowNodeDevice extends SpeechFlowNode {
             numerals:         true,
             paragraphs:       true,
             profanity_filter: true,
-            utterances:       false,
-        })
-        await new Promise((resolve) => {
-            this.dg!.on(Deepgram.LiveTranscriptionEvents.Open, () => {
-                this.log("info", "Deepgram: connection open")
-                resolve(true)
-            })
+            utterances:       false
         })
 
-        /*  hooks onto Deepgram API events  */
-        this.dg.on(Deepgram.LiveTranscriptionEvents.Close, () => {
-            this.log("info", "Deepgram: connection close")
-        })
+        /*  hook onto Deepgram API events  */
         this.dg.on(Deepgram.LiveTranscriptionEvents.Transcript, async (data) => {
             const text = data.channel?.alternatives[0].transcript ?? ""
             if (text === "")
                 return
             queue.emit("text", text)
         })
+        this.dg.on(Deepgram.LiveTranscriptionEvents.Metadata, (data) => {
+            this.log("info", "Deepgram: metadata received")
+        })
+        this.dg.on(Deepgram.LiveTranscriptionEvents.Close, () => {
+            this.log("info", "Deepgram: connection close")
+        })
         this.dg.on(Deepgram.LiveTranscriptionEvents.Error, (error: Error) => {
-            this.log("error", `Deepgram: ${error}`)
+            this.log("error", `Deepgram: ${error.message}`)
+            this.emit("error")
+        })
+
+        /*  wait for Deepgram API to be available  */
+        await new Promise((resolve) => {
+            this.dg!.once(Deepgram.LiveTranscriptionEvents.Open, () => {
+                this.log("info", "Deepgram: connection open")
+                resolve(true)
+            })
         })
 
         /*  provide Duplex stream and internally attach to Deepgram API  */
         const dg = this.dg
         this.stream = new Stream.Duplex({
-            write (chunk: Buffer, encoding: BufferEncoding, callback: (error?: Error | null | undefined) => void) {
+            write (chunk: Buffer, encoding, callback) {
                 const data = chunk.buffer.slice(chunk.byteOffset, chunk.byteOffset + chunk.byteLength)
                 if (data.byteLength === 0)
                     queue.emit("text", "")
@@ -83,19 +104,26 @@ export default class SpeechFlowNodeDevice extends SpeechFlowNode {
                     dg.send(data)
                 callback()
             },
-            read (size: number) {
+            read (size) {
                 queue.once("text", (text: string) => {
-                    if (text !== "")
-                        this.push(text)
+                    this.push(text)
                 })
+            },
+            final (callback) {
+                dg.requestClose()
             }
         })
     }
+
+    /*  close node  */
     async close () {
+        /*  close stream  */
         if (this.stream !== null) {
             this.stream.destroy()
             this.stream = null
         }
+
+        /*  shutdown Deepgram API  */
         if (this.dg !== null)
             this.dg.requestClose()
     }
