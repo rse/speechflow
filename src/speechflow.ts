3f632ecaed54a2b4a55dbc4db34c862c98fd28d1
Ralf S. Engelschall <rse@engelschall.com>
2025-04-27 14:46:26 +0200

    try to make NPM more happy

--- src/speechflow.ts
+++ src/speechflow.ts
@@ -1,4 +1,5 @@
-/*
+#!/usr/bin/env node
+/*!
 **  SpeechFlow - Speech Processing Flow Graph
 **  Copyright (c) 2024-2025 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
