c140ffddf5360cad9f5e93eaf6b24f776e418111
Ralf S. Engelschall <rse@engelschall.com>
2025-04-27 02:25:15 +0200

    cleanups

--- src/speechflow-node-ffmpeg.ts
+++ src/speechflow-node-ffmpeg.ts
@@ -14,6 +14,7 @@ import { Converter as FFmpegStream } from "ffmpeg-stream"
 /*  internal dependencies  */
 import SpeechFlowNode                from "./speechflow-node"
 
+/*  SpeechFlow node for FFmpeg  */
 export default class SpeechFlowNodeFFmpeg extends SpeechFlowNode {
     /*  internal state  */
     private ffmpegBinary = FFmpeg.supported ? FFmpeg.binary : "ffmpeg"
