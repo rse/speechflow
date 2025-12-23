ae820be96c9e030f996c439570574f8f7c6d803b
Ralf S. Engelschall <rse@engelschall.com>
2025-12-24 00:51:14 +0100

    add oxlint to UI sub-projects

--- speechflow-ui-st/src/lib.d.ts
+++
@@ -1,9 +0,0 @@
-/*
-**  SpeechFlow - Speech Processing Flow Graph
-**  Copyright (c) 2024-2025 Dr. Ralf S. Engelschall <rse@engelschall.com>
-**  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
-*/
-
-declare global {}
-export {}
-
